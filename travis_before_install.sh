c1f230b164cb366d4f67e11355bead41ee5c752b
KEINOS <github@keinos.com>
2020-02-20 19:25:44 +0900

    Update travis_before_install.sh

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -6,15 +6,3 @@ sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 sudo apt-get -y install tree
 sudo usermod -aG docker $USER
-
-echo '- Docker CLI Plugins:'
-tree ~/.docker/cli-plugins/
-
-echo '- Donwload Plugins:'
-mkdir -p ~/.docker/cli-plugins/docker-buildx
-cd ~/.docker/cli-plugins/docker-buildx && \
-curl -o docker-buildx https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.linux-amd64
-chmod a+x ~/.docker/cli-plugins/docker-buildx
-
-echo '- Installed Plugins:'
-tree ~/.docker/cli-plugins/
