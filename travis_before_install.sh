a8b11c6272d0800690e7564df7e6cb0df390dca5
KEINOS <github@keinos.com>
2020-02-20 19:15:52 +0900

    Update travis_before_install.sh

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -4,6 +4,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
+sudo apt-get -y install tree
 sudo usermod -aG docker $USER
 
 echo '- Docker CLI Plugins:'
