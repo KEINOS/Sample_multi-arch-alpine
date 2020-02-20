6c28c77fcc600b8f22d69c83850e98cec3cf7e55
KEINOS <github@keinos.com>
2020-02-20 19:13:31 +0900

    Update travis_before_install.sh

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -5,3 +5,20 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
 sudo usermod -aG docker $USER
+
+echo '- Docker CLI Plugins:'
+tree ~/.docker/cli-plugins/
+
+echo '- Donwload Plugins:'
+mkdir -p ~/.docker/cli-plugins/docker-buildx
+cd ~/.docker/cli-plugins/docker-buildx && \
+curl \
+    -O https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.darwin-amd64 \
+    -O https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.linux-amd64 \
+    -O https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.linux-arm-v6 \
+    -O https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.linux-arm-v7 \
+    -O https://github.com/docker/buildx/releases/download/v0.3.1/buildx-v0.3.1.linux-arm64
+chmod a+x ~/.docker/cli-plugins/docker-buildx
+
+echo '- Installed Plugins:'
+tree ~/.docker/cli-plugins/
