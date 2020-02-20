0915172bfccf9bc300b023cf8033a66e5e284576
KEINOS <github@keinos.com>
2020-02-20 18:59:07 +0900

    fix

--- build-image.sh
+++ build-image.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
-echo '- Docker Info'
+echo '====================='
+echo ' Docker Info'
+echo '====================='
+echo '- daemon.json: ';
 sudo cat /etc/docker/daemon.json
+echo '- Docker version:'
 docker version
+echo '- Docker BuildX version:'
 docker buildx version
-sudo docker run --rm --privileged linuxkit/binfmt
+echo '- Pull linuxkit/binfm:v0.7 (Latest on 2020/02/20)'
+docker pull linuxkit/binfmt:v0.7
+echo '- cat /proc/sys/fs/binfmt_misc/:'
 ls -l /proc/sys/fs/binfmt_misc/
 
+echo '- Login to Docker:'
 NAME_BUILDER='multibuilder'
 NAME_IMAGE="${DOCKER_USERNAME}/demo:latest"
-
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin || {
     echo 'You need to login Docker Cloud/Hub first.'
     exit 1
 }
 
-echo docker buildx ls
+echo '- docker buildx ls'
+docker buildx ls
 
 docker buildx ls | grep $NAME_BUILDER
 [ $? -ne 0 ] && {
+    echo '- Create builder: ' $NAME_BUILDER
     docker buildx create --name $NAME_BUILDER
 }
+echo '- Start build:'
 docker buildx use $NAME_BUILDER && \
 docker buildx inspect --bootstrap && \
 docker buildx build \
