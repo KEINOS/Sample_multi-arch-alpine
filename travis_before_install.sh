3c7d9c20e70d7970527289ec02c6884eac904eaa
KEINOS <github@keinos.com>
2020-02-20 19:43:03 +0900

    Update travis_before_install.sh

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+OS="$(lsb_release -cs)"
+DOCKER_APT_REPO="https://download.docker.com/linux/ubuntu"
+sudo add-apt-repository "deb [arch=amd64] $DOCKER_APT_REPO $OS stable"
 sudo apt-get update
+sudo apt-get -y install \
+    qemu-user-static \
+    binfmt-support
 sudo apt-get -y -o Dpkg::Options::="--force-confnew" install docker-ce
-sudo apt-get -y install tree
+
+echo '- Add user to docker group:'
 sudo usermod -aG docker $USER
+
+echo '- Check file system mount:'
+ls /proc/sys/fs/binfmt_misc/
