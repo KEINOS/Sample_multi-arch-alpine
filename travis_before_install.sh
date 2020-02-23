9e274218662cfe881e8c435d4ea2273cfdc33297
KEINOS <github@keinos.com>
2020-02-23 19:33:43 +0900

    fix: simple script

--- travis_before_install.sh
+++ travis_before_install.sh
@@ -12,33 +12,13 @@ sudo apt-get -y install \
     gnupg-agent \
     software-properties-common
 
-# =============================================================================
-#  Setup Repository
-# =============================================================================
-# Add Docker’s official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo '==============================================================================='
 
-# Visual verify
-sudo apt-key fingerprint 0EBFCD88
-echo '- Verify above with: 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88'
-
-# Set up the stable repository
-OS="$(lsb_release -cs)"
-DOCKER_APT_REPO="https://download.docker.com/linux/ubuntu"
-sudo add-apt-repository "deb [arch=amd64] $DOCKER_APT_REPO $OS stable"
+curl -fsSL get.docker.com -o get-docker.sh && \
+sh get-docker.sh && \
+sudo gpasswd -a $USER docker && \
+sudo docker run hello-world && \
+rm -f get-docker.sh
 
 # =============================================================================
-#  Install Docker Engine - Community
-# =============================================================================
-
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-echo '- Add user to docker group:'
-sudo usermod -aG docker $USER
-
-echo '- Docker version:'
-docker version
-
-echo '- Run hello-world:'
-docker run hello-world
+echo '[DONE]========================================================================='
